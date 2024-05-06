eed948cd5a69057643fa6e48394597e82cd728fa
Eflii <hugo.efligenir@gmail.com>
2024-05-06 21:20:38 +0200

    correction of protected system

--- back-end/utils/protected.js
+++ back-end/utils/protected.js
@@ -7,17 +7,20 @@ const loginURL = "http://localhost:8088/auth/loginPage"
 
 const protected = async (req, res, next) => {
   // get the token from the header
-  const authorization = req.headers["authorization"];
+
+  const authorization = req.cookies.accessToken;
+
   // if we don't have a token, return error
+  
   if (!authorization)
     return res.writeHead(301, {
         Location: loginURL
       }).end();
   // if we have a token, you have to verify it
-  const token = authorization.split(" ")[1];
+  // const token = authorization.split(" ")[1];
   let id;
   try {
-    id = verify(token, process.env.ACCESS_TOKEN_SECRET).id;
+    id = verify(authorization, process.env.ACCESS_TOKEN_SECRET).id;
   } catch {
     return res.writeHead(301, {
         Location: loginURL
